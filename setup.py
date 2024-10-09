55abf5800f7d9556586a541d02f826b30031723a
Jiri Vrany <jiri.vrany@cesnet.cz>
2024-10-09 09:47:17 +0200

    package name

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ExaBGP API",
+    name="exabgp_api",
     version="0.1.0",
     description="API for ExaBGP, started by ExaBGP process",
     author="Jiri Vrany",
     author_email="jiri.vrany@cesnet.cz",
     packages=find_packages(),
-    install_requires=[
-        "pika",
-        "python-dotenv",
-        "loguru",
-        "click",
-        "flask"
-    ],
-    python_requires='>=3.7',
+    install_requires=["pika", "python-dotenv", "loguru", "click", "flask"],
+    python_requires=">=3.7",
     entry_points={
         "console_scripts": [
             "exabgp-api=exabgp_api:main",
